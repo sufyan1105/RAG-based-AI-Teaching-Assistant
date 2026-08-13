6e19fb9c430ea142c25bd21348962e6bdb73ee05
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2026-08-13 13:30:17 +0200

    Checkpoint

--- read_chunks.py
+++ read_chunks.py
@@ -1,4 +1,7 @@
+# Forming embeddings of the jsons that we have created in the previous step. We will use the bge-m3 model to create embeddings of the text in the jsons.
+
 import requests
+import os
 import json
 
 def create_embedding(text):
@@ -9,3 +12,19 @@ def create_embedding(text):
     embedding = r.json()["embedding"]
     return embedding
 
+jsons = os.listdir("jsons/")
+my_dicts = []
+chunk_id = 0
+for json_file in jsons:
+    with open(f"jsons/{json_file}") as f:
+        content = json.load(f)
+    for chunk in content['chunks']:
+        chunk['chunk_id'] = chunk_id
+        chunk['embedding'] = create_embedding(chunk['text'])
+        chunk_id += 1
+        my_dicts.append(chunk)
+        print(chunk)
+    break
+
+
+print(my_dicts)
