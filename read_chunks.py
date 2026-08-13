994055864eb1836d16f7b712b9d03f402741e1bc
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2026-08-13 13:41:18 +0200

    embeddings done

--- read_chunks.py
+++ read_chunks.py
@@ -3,13 +3,14 @@
 import requests
 import os
 import json
+import pandas as pd
 
-def create_embedding(text):
-    r = requests.post("http://localhost:11434/api/embeddings", json = {
+def create_embedding(text_list):
+    r = requests.post("http://localhost:11434/api/embed", json = {
         "model": "bge-m3",
-        "prompt": text
+        "input": text_list
     })
-    embedding = r.json()["embedding"]
+    embedding = r.json()["embeddings"]
     return embedding
 
 jsons = os.listdir("jsons/")
@@ -18,13 +19,13 @@ chunk_id = 0
 for json_file in jsons:
     with open(f"jsons/{json_file}") as f:
         content = json.load(f)
-    for chunk in content['chunks']:
+    embeddings = create_embedding([c['text'] for c in content['chunks']])
+    for i, chunk in enumerate(content['chunks']):
         chunk['chunk_id'] = chunk_id
-        chunk['embedding'] = create_embedding(chunk['text'])
+        chunk['embedding'] = embeddings[i]
         chunk_id += 1
         my_dicts.append(chunk)
         print(chunk)
     break
-
 
 print(my_dicts)
