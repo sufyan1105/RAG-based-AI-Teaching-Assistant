51df72503c6b122da88c50cf06c9a58e2b0af74d
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2026-08-16 16:23:28 +0200

    Created a prompt for the LLM to use in future

--- process_incomming.py
+++ process_incomming.py
@@ -23,8 +23,23 @@ if __name__ == "__main__":
   similarities = cosine_similarity(
       np.vstack(df["embedding"].values), [question_embedding]
   ).flatten()
-  top_results = 3
+  top_results = 5
   max_indx = similarities.argsort()[::-1][0:top_results]
-  print(f"Similarities: {max_indx}")
+#   print(f"Similarities: {max_indx}")
   new_df = df.loc[max_indx]
-  print(new_df[["name", "text", "number"]])
+#   print(new_df[["name", "text", "number"]])
+
+prompt = f''' I am teaching web development using sigma web development course. Here are video chunks containing video names, text, number, start_time , end_time an the text at that time .:
+{new_df[["name", "text", "number", "start", "end"]].to_json}
+-----------------------------------------------------------
+" {incoming_query} "
+User asked this question related to the video chunks, you have to where and how much content is taught in which video (in which video and at what timestamp) and guide the user to go to that part. If user asks unrelated questions tell him you can only answer questions related to the Course. 
+'''
+
+  for index, row in new_df.iterrows():
+    print(f"Name: {row['name']}")
+    print(f"Text: {row['text']}")
+    print(f"Number: {row['number']}")
+    print(f"start_time: {row['start']}")
+    print(f"end_time: {row['end']}")
+    print("--------------------------------------------------")
